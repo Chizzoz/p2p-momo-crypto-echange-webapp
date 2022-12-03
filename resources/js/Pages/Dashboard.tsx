8fcc938e186b9978ce729ba4ce3466296f7a7a3a
Chinyanta Mwenya <chizzo84@gmail.com>
2022-12-03 20:26:10 +0200

    Update Dashboard.tsx

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -55,9 +55,11 @@ export default function Dashboard({ alchemy, walletAddress, setWalletAddress, pr
                 ({ walletAddress }: Props) => (
                     <div className="py-12">
                         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                            <div className="p-6 sm:px-20 bg-white border-b border-gray-200">
-                                <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                                    <Welcome address={walletAddress} />
+                            <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                                <div className="p-6 sm:px-20 bg-white border-b border-gray-200">
+                                    <div className="grid grid-cols-1 md:grid-cols-3">
+                                        <Welcome address={walletAddress} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
