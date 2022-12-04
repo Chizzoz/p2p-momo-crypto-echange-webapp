fe3d03254af9166e44515793e7bf84e0c6733430
Chinyanta Mwenya <chizzo84@gmail.com>
2022-12-05 00:53:17 +0200

    Reconcile Button

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -116,6 +116,16 @@ export default function Dashboard() {
                                         }
                                     </div>
                                 </div>
+                                {
+                                    page.props.user.id == 1 &&
+                                    <button
+                                        type="button"
+                                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                        onClick={() => reconcileTransactions()}
+                                    >
+                                        Reconcile
+                                    </button>
+                                }
                             </div>
                         </div>
                     </div>
